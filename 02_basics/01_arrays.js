5574cd683a20b9fc5709c6460db2f4c7d1eb8589
Himanshu Yadav <162490837+yadav0507@users.noreply.github.com>
2025-03-29 16:44:24 +0000

    basics of array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+//arrays
+
+const myArr = [1,2,3,4,5,6,7];
+
+console.log(myArr)
+const myHeros = ["shaktiman", "spiderman", "Krish"];
+//console.log(myHeros)
+// myArr.push(34)
+// console.log(myArr)
+// myArr.pop()
+// console.log(myArr)
+
+//myArr.unshift()
+//myArr.shift()
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+// console.log(typeof newArr)
+
+
+
+                                //Slice and splice
+
+
+
+// console.log("A", myArr);
+
+// const myn1 = myArr.slice(1,3);
+
+// console.log("B", myArr);
+
+
+
+
+
+
+
