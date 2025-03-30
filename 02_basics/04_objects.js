7fe908b80427cd7bddb95e5fe17fa9624d4e5825
Himanshu Yadav <162490837+yadav0507@users.noreply.github.com>
2025-03-30 09:34:42 +0000

    Objects and methods

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,54 @@
+const tinderUser = new Object() // singleton object
+const tinderUser2 = {}           // non singleton object
+
+tinderUser.id = "123csx"
+tinderUser.name = "samm"
+tinderUser.isLoggedIn = "false"
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullName: {
+        userNmae: {
+            firstName: "Himanshu",
+            lastNmae: "yadav",
+        }
+    }
+}
+
+
+//console.log(regularUser.firstName)
+
+const obj1 = {1: "a", b: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+//const obj3 = {obj1, obj2}
+//const obj3 = Object.assign({} ,obj1, obj2)
+//console.log(obj3)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3)
+
+const users = [
+    {
+        id: 1,
+        email: "him@gmail.com"
+    },
+    {
+        id: 1,
+        email: "him@gmail.com"
+    },
+    {
+        id: 1,
+        email: "him@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"))
+
