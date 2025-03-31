0aa8842c2c5d0b4a4ddf4e234225fbcdcbb17c4f
Himanshu Yadav <162490837+yadav0507@users.noreply.github.com>
2025-03-31 18:31:33 +0000

    vests

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -31,11 +31,44 @@ const result = addTwoNumber(2, 5) // arguments
 function loginUser(userName = "samm"){
     if(!userName)
     {
-        console.log("please enter a username");
+        //console.log("please enter a username");
         return
     }
     return `${userName} just logged in`
 
 }
-console.log(loginUser());
+//console.log(loginUser());
+
+
+function calculateCartPrice(val1, val2, ... num1){ // ... rest operator
+    
+    return num1
+
+}
+//console.log(calculateCartPrice(2,345,7654))
+
+
+
+const user = {
+    userName : "himanshu",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.userName} and price is ${anyobject.price} `)
+}
+
+// handleObject(use)
+handleObject({
+    username: "samm",
+    priceL 999
+})
+
+const myNewArry = [200, 300, 100, 900]
+function retrunSecondValue(getArry){
+    return getArry[1]
+}
+
+
+
 
