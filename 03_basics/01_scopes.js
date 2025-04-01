8b759a22156b6ff4b7cb1786f75dc96e20e4e9c2
Himanshu Yadav <162490837+yadav0507@users.noreply.github.com>
2025-04-01 07:34:52 +0000

    nested functions

--- 03_basics/01_scopes.js
+++ 03_basics/01_scopes.js
@@ -5,14 +5,57 @@ if(true){
     let a = 10;
     const b = 20
    
-    console.log("inner: ", a);
+    //console.log("inner: ", a);
 }
 
-for()
 
 
-    
-console.log(a)
+
+
+//console.log(a)
 
 //console.log(b)
 //console.log(c)
+
+
+
+// nested scope
+
+function one(){
+    const username = "himansu"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website)
+   // two()
+}
+one()
+
+if(true){
+    const username = "himanshu"
+    if(username === "himanshu"){
+        const website = " yadav"
+        // console.log(username + website)
+    }
+    // console.log(website)
+
+}
+//console.log(username)
+
+
+// ============================= interesting ========================
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+
+
+}
+
+
+addTwo(3)
+const addTwo = function(num){
+    return num + 2;
+}
