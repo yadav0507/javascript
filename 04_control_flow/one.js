5268599690ae939397929486e2cf5b77c0b712f9
Himanshu Yadav <162490837+yadav0507@users.noreply.github.com>
2025-04-03 19:31:42 +0000

    control flow in js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,31 @@
+// // if
+// const isuserin = true
+// const temp = 41
+
+// if(temp < 50){
+//     console.log("less then 50");
+
+// }
+// else{
+//     console.log("temmp is geater");
+// }
+// console.log('omm');
+// //<, >, <=, ==, !=, ===, 
+
+// const score = 200
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+
+const balance = 1000
+
+// if(balance > 900) console.log("ts")
+// console.log("ts2");
+
+if(balance > 400){
+    console.log("less than")
+    
+}
