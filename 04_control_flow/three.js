1ac858b7172c3e93e73ba7f03ba6fc942a649242
Himanshu Yadav <162490837+yadav0507@users.noreply.github.com>
2025-04-13 08:23:23 +0000

    explored maps and loops

---
+++ 04_control_flow/three.js
@@ -0,0 +1,65 @@
+// // for of loop
+
+// ["", ""]
+
+// [{},{}, []]
+
+// const arr = [1,2,4,5,7]
+
+// for(const num of arr){
+//     console.log(num);
+// }
+
+// const greetings = "Himee"
+// for(const gree of greetings){
+//     console.log(`each char is ${gree}`)
+// }
+
+// // Maps
+
+// const map = new Map()
+// map.set('IN', "India");
+// console.log(map)
+
+// for(const [i, j] of map){
+//     console.log(i, ':-', j)
+// }
+
+// const myobj = {
+//     'game': 'NFS',
+//     'game': 'PUBG'    
+// }
+
+// for(const [key, value] of myobj)
+// {
+//     console.log(key, ':-', value)
+// }
+
+
+//===================================  0bj =================================
+
+const myObj = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby"
+}
+
+// for in loop
+
+for(const key in myObj){
+    console.log(myObj[key])
+}
+
+
+const programming = ["js", "java", "cpp"]
+
+
+
+// for each loop
+
+
+const coding = ["js", " java", "ruby"]
+
+coding. .forEach(element => {
+    
+});
---
+++ 04_control_flow/two.js
@@ -0,0 +1,32 @@
+// let indexx = 0
+// while(indexx <= 10){
+//     console.log(`value of index is ${indexx}`);
+//     indexx = indexx+2;
+// }
+
+// let myArr = ['flash', 'batman' ,'superman']
+
+// let arr = 0
+// while(arr < myArr.length){
+//     console.log(`value is ${myArr[arr]}`)
+//     arr = arr + 1;
+// }
+
+
+// let score = 111;
+// do{
+//     console.log(`score is ${score}`);
+//     score++;
+// }while(score < 20)
+
+
+// for of
+
+
+const arr = [1,2,3,4,5,6]
+
+for (const num of arr) {
+
+    console.log(num);
+    
+}
